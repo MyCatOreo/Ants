4830f98d63cd1b31db1edd9d3969dd38b6a2add3
Lily Yang <neixingren@gmail.com>
2020-03-05 22:11:49 +1100

    Updates

---
+++ precache-manifest.d495db853067fc46619559847ea3ed5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60ac89a0e59e8a10db103b68ffb2bbcd",
+    "url": "/MyCatOreo/Ants/index.html"
+  },
+  {
+    "revision": "1c67e4ba4e930bfa94ca",
+    "url": "/MyCatOreo/Ants/static/css/main.095ea654.chunk.css"
+  },
+  {
+    "revision": "bfed4ab6507960fe3246",
+    "url": "/MyCatOreo/Ants/static/js/2.56cf7c2e.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/MyCatOreo/Ants/static/js/2.56cf7c2e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1c67e4ba4e930bfa94ca",
+    "url": "/MyCatOreo/Ants/static/js/main.882b86fa.chunk.js"
+  },
+  {
+    "revision": "2ff1828535bce7fcb89a",
+    "url": "/MyCatOreo/Ants/static/js/runtime-main.365eb30a.js"
+  }
+]);
